c62917e849640212d632e1fa7e5d831a0faf654a
Adam Sir <Adam.sir@servian.com>
2020-04-14 12:00:55 +1000

    Uploaded conference demo

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -0,0 +1,32 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/sessions")
+public class SessionsController {
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list() {
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id) {
+        return sessionRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session) {
+        return sessionRepository.saveAndFlush(session);
+    }
+}
